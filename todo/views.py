90813376650fa5ebfc0e59096581961fb9f2ba98
Deeya99902 <git config --global user.email>
2026-06-30 00:45:31 +0545

    Added signup functionality

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.shortcuts import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+
+def home(request):
+    return render(request,'home.html')
+
+def login(request):
+    return render(request,'login.html')
+
+def signup(request):
+    if request.method == 'GET':
+        form = UserCreationForm()
+        context ={
+         "form":form
+        }
+        return render(request,'signup.html', context=context)
+
+    else:
+        print(request.POST)
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            return HttpResponse("Form is valid")
+        else:
+            return HttpResponse("Form is invalid")
+    
+
+
+   
